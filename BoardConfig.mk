e97bd9c72a6fe9e0f8ed8dc3c2e53499fe459f34
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-03-08 19:45:07 +0000

    Added missed library for decryption, and disable OTG functions.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := Infinix-X671,Infinix-X671B
+TARGET_OTA_ASSERT_DEVICE := Infinix-X671,X671
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := mt6833
@@ -118,11 +118,8 @@ AB_OTA_PARTITIONS += \
     product \
     system_ext \
     boot \
-    vbmeta \
     vbmeta_vendor \
-    vbmeta_system \
-    dtbo \
-    lk
+    vbmeta_system
 
 # Platform
 TARGET_BOARD_PLATFORM := $(PRODUCT_PLATFORM )
@@ -176,10 +173,10 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 #TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_SCREEN_BLANK := true
 #TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_EXCLUDE_APEX := true
 TARGET_USES_MKE2FS := true # Use mke2fs to create ext4 images
 TW_HAS_MTP := true
-TW_LOAD_VENDOR_MODULES := "tran_fre.ko wmt_drv.ko met.ko connfem.ko fpsgo.ko"
+#TW_LOAD_VENDOR_MODULES := "tran_fre.ko wmt_drv.ko met.ko connfem.ko fpsgo.ko"
+TW_INCLUDE_NTFS_3G := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
@@ -190,14 +187,21 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
 # Excludes
 TW_EXCLUDE_TWRP_APP := true
+TW_EXCLUDE_APEX := true
 
 # Include some binaries
-#TW_INCLUDE_LIBRESETPROP := true
+TW_INCLUDE_LIBRESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
-#TW_INCLUDE_BASH := true
+TW_INCLUDE_BASH := true
 
 # Maintainer/Version
 TW_DEVICE_VERSION := perilouspike
